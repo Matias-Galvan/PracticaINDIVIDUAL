71978ab556ca2cc3860f757edaef33251e75f419
Matias-Galvan <matiasgalvan90@gmail.com>
2023-10-03 23:39:15 -0300

    tocando el funcionCommand

--- Data/Command/Funciones/FuncionCommand.cs
+++ Data/Command/Funciones/FuncionCommand.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Interfaces.Funciones;
 using Domain.Entities;
 using Infraestructure.Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace Infraestructure.Command.Funciones
 {
     public class FuncionCommand: IFuncionCommand
